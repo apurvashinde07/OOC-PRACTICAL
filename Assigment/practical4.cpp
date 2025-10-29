7b3ac3e8361226d1ff5e1a2f1faa7a9a1275edb4
Apurva shinde <apurvashinde077@gmail.com>
2025-10-29 23:09:59 +0530

    Add files via upload

---
+++ Assigment/practical4.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+  class Rectangle{
+private:
+  double length;
+  double width;
+public:
+  Rectangle():length(1.0), width(1.0){}
+  Rectangle(double len,double wid):length(len),width(wid){}
+  ~Rectangle(){
+      cout<<"Rectangle object destroyed."<<endl;
+  }
+  double getlength()const{
+    return length;
+  }
+  double getwidth(){
+    return width;
+  }
+  void setlength(double len){
+    length=len;
+  }
+  void setwidth(double wid){
+    width=wid;
+  }
+  double calculateArea()const{
+    return length*width;
+  }
+  double calculatePerimeter()const{
+    return 2*(length+width);
+  }
+  };
+  int main(){
+    Rectangle rect(4.0,40.0);
+    cout<<"Rectangle Properties: "<<endl;
+    cout<<"Length: "<<rect.getlength()<<endl;
+    cout<<"Width: "<<rect.getwidth()<<endl;
+    cout<<"Area: "<<rect.calculateArea()<<endl;
+    cout<<"Perimeter: "<<rect.calculatePerimeter()<<endl;
+
+    return 0;
+  }
+
+//output:
+
+// Rectangle Properties: 
+// Length: 4
+// Width: 40
+// Area: 160
+// Perimeter: 88
+// Rectangle object destroyed.
