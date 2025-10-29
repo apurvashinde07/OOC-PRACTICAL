7e9f742500a1aa587f35464df79d9e2e9e8630a9
Apurva shinde <apurvashinde077@gmail.com>
2025-10-29 23:24:17 +0530

    Add files via upload

---
+++ Assigment/practical82.cpp
@@ -0,0 +1,36 @@
+// C++ program to show binary operator overloading
+#include <iostream>
+using namespace std;
+
+class Distance {
+    int feet, inch;
+public:
+    Distance() {}
+    Distance(int f, int i) { feet = f; inch = i; }
+    Distance operator + (Distance d2);
+    void display() {
+        cout << feet << " Feet & " << inch << " Inch" << endl;
+    }
+};
+
+Distance Distance::operator+(Distance d2) {
+    Distance d3;
+    d3.feet = feet + d2.feet;
+    d3.inch = inch + d2.inch;
+    return d3;
+}
+
+int main() {
+    Distance d1(18, 9);
+    Distance d2(12, 4);
+    Distance d3;
+    d3 = d1 + d2;
+    cout << "After overloaded operator" << endl;
+    d3.display();
+    return 0;
+}
+
+//output:
+
+// After overloaded operator
+// 30 Feet & 13 Inch
