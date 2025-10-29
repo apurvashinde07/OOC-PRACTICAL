fbfc2682d1fe73dd5354caf25f132b5c8ccd8f2c
Apurva shinde <apurvashinde077@gmail.com>
2025-10-29 23:07:04 +0530

    Add files via upload

---
+++ Assigment/practical1.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class student{
+    private:
+    char name[30];
+    int rollno;
+    float marks;
+    public:
+    void inputdetails(){
+        cout<<"Enter Student name: ";
+        cin>>name;
+        cout<<"enter roll number: ";
+        cin>>rollno;
+        cout<<"Enter marks: ";
+        cin>>marks;
+    }
+
+    void displaydetails(){
+        cout<<"student name: "<<name<<endl;
+        cout<<"roll number: "<<rollno<<endl;
+        cout<<"marks: "<<marks<<endl;
+    }
+
+};
+int main(){
+    student studentObj;
+    studentObj.inputdetails();
+    cout<<"\n student Details: \n";
+    studentObj.displaydetails();
+    return 0;
+}
+
+
+//output
+
+// Enter Student name: Apurva
+// enter roll number: 85
+// Enter marks: 89
+
+//  student Details: 
+// student name: Apurva
+// roll number: 85
+// marks: 89
